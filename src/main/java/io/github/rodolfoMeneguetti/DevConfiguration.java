52ac158d35b3c1acdb79d5cd4e43f0f3435d2672
RodofoMeneguetti <paulo.meneguettitec@gmail.com>
2023-02-23 16:28:31 -0300

    Configuration Dependency H2

--- src/main/java/io/github/rodolfoMeneguetti/DevConfiguration.java
+++ src/main/java/io/github/rodolfoMeneguetti/DevConfiguration.java
@@ -1,12 +1,11 @@
 package io.github.rodolfoMeneguetti;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 
 @Development
 public class DevConfiguration {
 
-    @Bean
+
     public CommandLineRunner executar(){
         return args -> {
             System.out.println("====================================");
