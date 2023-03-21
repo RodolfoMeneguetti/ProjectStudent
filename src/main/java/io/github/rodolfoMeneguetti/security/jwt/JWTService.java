c6cc1f0ef760a372de02a2eca0d83d3a2ea8e2f4
RodofoMeneguetti <paulo.meneguettitec@gmail.com>
2023-03-21 01:39:23 -0300

    Adding method JwtAuthFilter

--- src/main/java/io/github/rodolfoMeneguetti/security/jwt/JWTService.java
+++ src/main/java/io/github/rodolfoMeneguetti/security/jwt/JWTService.java
@@ -1,6 +1,9 @@
-package io.github.rodolfoMeneguetti;
+package io.github.rodolfoMeneguetti.security.jwt;
 
+import io.github.rodolfoMeneguetti.VendasApplication;
 import io.github.rodolfoMeneguetti.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,6 +40,30 @@ public class JWTService {
 
     }
 
+    public Claims obterClaims( String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido (String token){
+        try{
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
+    }
+
     public static void main (String[] args){
         ConfigurableApplicationContext context =  SpringApplication.run(VendasApplication.class);
         JWTService service =(JWTService) context.getBean(JWTService.class);
@@ -45,6 +72,10 @@ public class JWTService {
         System.out.println("Gerar Token  ------------------  ");
         System.out.println(token);
 
+        boolean isTokenValido = service.tokenValido(token);
+        System.out.println(" O TOKEN ESTA VALIDO ? : " + isTokenValido);
+
+        System.out.println(service.obterLoginUsuario(token));
     }
 
 }
