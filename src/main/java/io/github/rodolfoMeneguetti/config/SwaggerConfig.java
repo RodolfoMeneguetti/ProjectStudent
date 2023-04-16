2ed1d4b18cc37289bce561af601eb3756e6fcbb5
RodofoMeneguetti <paulo.meneguettitec@gmail.com>
2023-04-16 15:53:47 -0300

    include PDBBOX dependency

--- src/main/java/io/github/rodolfoMeneguetti/config/SwaggerConfig.java
+++ src/main/java/io/github/rodolfoMeneguetti/config/SwaggerConfig.java
@@ -1,7 +1,62 @@
 package io.github.rodolfoMeneguetti.config;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("io.github.rodolfoMeneguetti.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("API projeto de vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Paulo Rodolfo Meneguetti",
+                "https://github.com/RodolfoMeneguetti",
+                "paulo.meneguetti@hotmail.com");
+    }
+
+    public ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header" );
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "acessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
+    }
+
 }
